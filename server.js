c35ecb7d3f02999910afe99e84f062293e28149a
omeiregman <=>
2018-02-25 10:34:29 +0100

    Added cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // server.js
 var app = require('./app');
+var cors = require('cors');
+
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
+
+app.use(cors());
