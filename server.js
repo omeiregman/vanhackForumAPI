c8e5342a83a52aadff16ba25ca65ed8bb376f817
omeiregman <=>
2018-03-02 16:46:35 +0100

    modified cors

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
