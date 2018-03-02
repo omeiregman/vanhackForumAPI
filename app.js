4f2c7db0cf035a17d23d818cdb8274411f7f2c00
omeiregman <=>
2018-03-02 19:41:24 +0100

    modified cors

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app = express();
 var db = require('./db');
 
 var PostController = require('./Post/PostController');
+
 app.use('/posts', PostController);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -6,11 +6,3 @@ var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
-
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
